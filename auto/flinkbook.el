b640bbe5a0a6134945b33465de5b3dcc64b654ec
confucianzuoyuan <zuoyuantc@126.com>
2020-11-16 16:19:05 +0800

    udpate

---
+++ auto/flinkbook.el
@@ -0,0 +1,14 @@
+(TeX-add-style-hook
+ "flinkbook"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("elegantbook" "cn" "11pt" "chinese")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "elegantbook"
+    "elegantbook11"
+    "array")
+   (TeX-add-symbols
+    '("ccr" 1)))
+ :latex)
+
